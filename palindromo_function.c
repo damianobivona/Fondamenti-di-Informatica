2eb1ea6b0759fd68097b2c80678332c836d4e1ab
Damiano-Mac <damiano@mac.it>
2025-01-29 18:59:39 +0100

    mod

--- palindromo_function.c
+++ palindromo_function.c
@@ -1,2 +1,42 @@
 #include <stdio.h>
 
+#define LMAX 25
+
+int pal(char, int);
+
+int main() {
+
+    char word[LMAX];
+    char w;
+    int a = 0;
+
+    printf("Inserisci una parola: ");
+
+    while ( getchar() = w) {
+        word[a] = w;
+        a++;
+    }
+    
+    word[i] = '\0'
+
+    if (pal(word, i)) {
+        printf("La parola (%s) NON è palindroma!", word);
+    }
+    printf("La parola (%s) è palindroma!", word);
+}
+
+int pal(char word[], int i) {
+
+    int s = 0;
+    int e = s - 1;
+
+    while ( s < e) {
+        if (word[s] != word[e]) {
+            return 0;
+        }
+        s++;
+        e--;
+    }
+    return 1;
+
+}
