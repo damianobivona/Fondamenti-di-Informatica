e918def2b3b2dfa48dc2d0367cbceb328cce193d
Damiano-Mac <damiano@mac.it>
2025-02-19 11:02:20 +0100

    lez. 10 - ARRAY

--- input_two_array_10.c
+++ input_two_array_10.c
@@ -4,16 +4,46 @@ dellʼarray e unʼaltra per individuare il valore minimo. Entrambe le funzioni d
 parametri lʼarray e il numero di elementi. Dopo aver trovato massimo e minimo, stampa i due valori.*/
 
 #include <stdio.h>
-#define MAX 25
 
-int maxarray(int, int);
-int minarray(int, int);
+int maxarray(int[], int);
+int minarray(int[], int);
 
 int main() {
-    int arr1[MAX];
+    int n;
+
+    // utente inserisce la grandezza dell'array
+    printf("inserisci la grandezza dell'array: ");
+    scanf("%d", &n);
+
+    int arr1[n];
+    for (int i = 0; i < n; i++) {
+        printf("inserisci un numero: ");
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Num Max: %d", maxarray(arr1, n));
+    printf("\nNum Min: %d", minarray(arr1, n));
 }
 
-int maxarray(int) {
+int maxarray(int arrmax[], int a) {
+    int max = arrmax[0];
 
+    for (int i = 1; i < a; i++) {         // "1" è il secondo valore dell'array; "n" è la grandezza dell'array
+        if (arrmax[i] > max) {
+            max = arrmax[i];
+        }
+    }
+    return max;
+}
+
+int minarray(int arrmin[], int b) {
+    int min = arrmin[0];
+
+    for (int i = 1; i < b; i++) {         // "1" è il secondo valore dell'array; "n" è la grandezza dell'array
+        if (arrmin[i] < min) {
+            min = arrmin[i];
+        }
+    }
+    return min;
 }
 
