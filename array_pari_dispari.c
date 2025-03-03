27f17901822546be67f17a0d57fa8266b57e26a5
Damiano-Mac <damiano@mac.it>
2025-03-03 19:12:48 +0100

    PARI_DISPARI

--- array_pari_dispari.c
+++ array_pari_dispari.c
@@ -6,18 +6,27 @@ int controll(int[], int);
 
 int main() {
 
-}
+    int n;
+    printf("Inserisci il numero di elementi: ");
+    scanf("%d", &n);
 
-int controll(int v[], int x) {
-    for ( int i = 0; i < x; i++) {
-        if (v[i] % 2 == 0) { // se è PARI (deve controllare se il seguente è dispari)
-            if (v[i+1] % 2 != 0) { // se il seg. è DISPARI
-            }
-        }
-        else if (v[i] % 2 != 0) { // controllo se è DISPARI
-            if (v[i] % 2 == 0) { // se il seguente è PARI
-            }
-        }
+    int v[n];
+    for (int i = 0; i < n; i++) {
+        printf("Inserisci elemento %d: ", i);
+        scanf("%d", &v[i]);
+    }
+
+    if (controll(v, n)) {
+        printf("I numeri sono alternati");
+    } else {
+        printf("I numeri non sono alternati");
     }
 }
 
+int controll(int v[], int x) {
+    for ( int i = 0; i < x - 1; i++) {
+        if (v[i] % 2 == v[i + 1] % 2) { // controlla se sono consecutivi due pari
+            return 0;
+        }
+    } return 1;
+}
